d96699cd185f15759507998175f692b958bdd4b5
Michael Di Cesare <mvdicesare@gmail.com>
2019-10-18 09:03:11 -0600

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Michael Di Cesare on 10/18/19.
+//  Copyright © 2019 Michael Di Cesare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
